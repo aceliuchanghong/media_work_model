f17f604115b2c02af944f1fbbd56b02a2e36679c
Lawrence-AIP <liuch@allinfinance.com>
2024-01-22 11:09:27 +0800

    push

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ current_file_path = os.path.abspath(__file__)
 # 获取当前文件所在的目录
 current_dir = os.path.dirname(current_file_path)
 path0 = r'D:\aprojectPython\pythonProject\bili_douyin_xhs_uploader'
+
 print_tree(path0, exclude_dirs={'test', '__init__.py'})
